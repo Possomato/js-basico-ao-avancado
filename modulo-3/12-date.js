cf0126a1cec872651bf0d58528ca38d2e6c156c3
Gabriel Bonifacio Possomato <gabrielb.possomato@gmail.com>
2023-09-22 20:23:19 -0300

    add class 13

--- modulo-3/12-date.js
+++ modulo-3/12-date.js
@@ -1,4 +1,4 @@
 // aula 12 - mod 3
 
-
-
+const data = new Date() // uma função construtora
+console.log(data.toString())
---
+++ modulo-3/13-switch.js
@@ -0,0 +1,31 @@
+// aula 13 - mod 3
+
+const data = new Date('1987-04-24 00:00:00')
+const diaSemana = data.getDay()
+let diaSemanaTexto
+
+switch (diaSemana) {
+  case 0:
+    diaSemanaTexto = 'Domingo'
+    break
+  case 1:
+    diaSemanaTexto = 'Segunda'
+    break
+  case 2:
+    diaSemanaTexto = 'Terça'
+    break
+  case 3:
+    diaSemanaTexto = 'Quarta'
+    break
+  case 4:
+    diaSemanaTexto = 'Quinta'
+    break
+  case 5:
+    diaSemanaTexto = 'Sexta'
+    break
+  case 6:
+    diaSemanaTexto = 'Sábado'
+    break
+}
+
+console.log(diaSemanaTexto)
