ef5c7e80d5c3fb12cbd863b74def7d48148b774d
Gabriel Bonifacio Possomato <gabrielb.possomato@gmail.com>
2023-10-04 16:01:53 -0300

    add class 8

---
+++ modulo-5/08-foreach.js
@@ -0,0 +1,9 @@
+// aula 08 - mod 5
+
+// forEach: é como um 'for' simplificado, a diferença dele para o 'map' é que não tem o retorno
+
+const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27]
+
+numeros.forEach((valor, indice, array) =>{
+  console.log(valor, indice)
+})
